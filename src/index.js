9906020793fed4a3d20fd043adb0295a5a3e5c81
i.sapozhkov <i.sapozhkov.93@gmail.com>
2020-01-23 21:35:32 +0300

    [backend] Add simple data searcher

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const compression = require('compression');
 
+const { searchSolution } = require('./services/search');
+
 const port = process.env.PORT || 8080;
 const app = express();
 app.use(bodyParser.json({ limit: '5mb' }));
@@ -10,7 +12,10 @@ app.use(compression());
 app.use(cors());
 
 app.get("/search", (req, res) => {
-    res.send("test");
-  });
+  const { query: { q } } = req;
+  if (!q) res.status(404).json({ error: 'Not found' });
+
+  res.send(searchSolution(q));
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
---
+++ src/services/search.js
@@ -0,0 +1,38 @@
+const fuse = require('fuse.js');
+const each = require('lodash.foreach');
+const data = require('./data/sample-data.json');
+
+/**
+ * data format is:
+ * {
+ *   key1: [{
+ *     "symptom": string,
+ *     "solution": string[],
+ *   }],
+ *   key2: [...],
+ *   ...
+ * }
+ */
+const options = {
+  shouldSort: true,
+  includeScore: true,
+  threshold: 0.5,
+  location: 0,
+  distance: 200,
+  maxPatternLength: 32,
+  minMatchCharLength: 1,
+  keys: [
+    "symptom"
+  ]
+};
+
+function searchSolution(problem) {
+  const res = [];
+  each(data, (v, k) => {
+    const f = new fuse(v, options);
+    res.push(...f.search(problem).map(v => ({ tag: k, score: v.score, ...v.item })));
+  });
+  return res;
+}
+
+module.exports = { searchSolution };
